c8689a990e608b483b52833e0f5f18490b64be1c
Carla Madeo <carlamadeo@gmail.com>
2022-08-02 20:11:06 -0300

    05-Bundlers completed

--- 05-Bundlers/homework/browser/app.js
+++ 05-Bundlers/homework/browser/app.js
@@ -1,29 +1,28 @@
-(function () {
+import { draw } from './whiteboard';
+import { io } from 'socket.io-client';
 
-  var whiteboard = window.whiteboard;
-  var socket = window.io(window.location.origin);
+let socket = io(window.location.origin);
 
-  socket.on('connect', function () {
-    console.log('Connected!');
+socket.on('connect', function () {
+  console.log('Connected!');
+});
+
+on('load', function (strokes) {
+
+  strokes.forEach(function (stroke) {
+    var start = stroke.start;
+    var end = stroke.end;
+    var color = stroke.color;
+    draw(start, end, color, false);
   });
 
-  socket.on('load', function (strokes) {
+});
 
-    strokes.forEach(function (stroke) {
-      var start = stroke.start;
-      var end = stroke.end;
-      var color = stroke.color;
-      whiteboard.draw(start, end, color, false);
-    });
+on('draw', function (start, end, color) {
+  draw(start, end, color, false);
+});
 
-  });
+on('draw', function (start, end, color) {
+  emit('draw', start, end, color);
+});
 
-  socket.on('draw', function (start, end, color) {
-    whiteboard.draw(start, end, color, false);
-  });
-
-  whiteboard.on('draw', function (start, end, color) {
-    socket.emit('draw', start, end, color);
-  });
-
-})();
